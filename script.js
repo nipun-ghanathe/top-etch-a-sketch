8110c717d5f9188e149095a94f3fdd4ae9e60908
nipun <nipuncod@gmail.com>
2026-03-12 10:56:43 +0530

    Version 2

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ function drawGrid(gridDiv, gridSize = 16) {
     for (let i = 0; i < gridSize; i++) {
       let cellDiv = document.createElement('div');
       cellDiv.classList.add('cell');
+      cellDiv.addEventListener('mouseenter', () => {
+        const bgc =
+          getComputedStyle(cellDiv).getPropertyValue('background-color');
+        const colorParts = bgc.match(/[\d.]+/g).map(Number);
+        let [r, g, b, a = 1] = colorParts;
+        if (a !== 1)
+          cellDiv.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a + 0.1})`;
+      });
       rowDiv.appendChild(cellDiv);
     }
 
@@ -17,7 +25,7 @@ const wrapper = document.querySelector('.wrapper');
 const grid = document.querySelector('.grid');
 
 const btn = document.querySelector('button');
-btn.addEventListener('click', function () {
+btn.addEventListener('click', () => {
   let gridSize = prompt('Enter grid size (btw 1 and 100)');
   if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
     console.log('Invalid grid size');
